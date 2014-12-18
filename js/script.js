65c463505234f390bf68d2051c010e6e89356295
Robert Pamfile <robert.pamfile@gmail.com>
2014-12-18 17:53:03 +0200

    side pages effect

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-window.onload = function() {
 	var container = document.querySelector('#splitlayout');
 	var leftZone = document.querySelector('#left');
 	var rightZone = document.querySelector('#right');
+	var backLeft = document.querySelector(".back-left");
+	var backRight = document.querySelector(".back-right");
+window.onload = function() {
+	
+	backLeft.addEventListener('click', function() {
+		container.className = 'splitlayout';
+	});
 
+	backRight.addEventListener('click', function() {
+		container.className = 'splitlayout';
+	});
 
 	leftZone.addEventListener('click', function(){
 		container.className = 'splitlayout left-open';
@@ -12,7 +21,3 @@ window.onload = function() {
 		container.className = 'splitlayout right-open';
 	});
 };
-
-function toggle(){
-	// code ..
-}
